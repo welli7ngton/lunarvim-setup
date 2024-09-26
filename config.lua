38dde82fe511bc32929f81f5fc141c4fa599392b
Wellington - Trabalho <wellingtonasilva45@gmail.com>
2024-09-26 19:41:13 -0300

    initial config files for my setup

---
+++ config.lua
@@ -0,0 +1,13 @@
+-- Importa os módulos de configuração personalizados
+require("welli7ngton.options")     -- Configurações gerais
+require("welli7ngton.plugins")     -- Plugins
+require("welli7ngton.colorscheme") -- Tema de cores
+require("welli7ngton.autocmds")    -- Comandos automáticos
+require("welli7ngton.filetype")    -- Configurações de tipos de arquivos
+require("welli7ngton.kind")        -- Configurações específicas (depende do conteúdo)
+require("welli7ngton.dashboard")   -- Configuração de dashboard e banners
+require("welli7ngton.which-key")
+require("welli7ngton.autocmds")
+-- Configurações gerais do LunarVim
+lvim.log.level = "warn"
+lvim.format_on_save = true
---
+++ lua/welli7ngton/keymaps.lua
@@ -0,0 +1,35 @@
+local kind = require('welli7ngton.kind')
+
+lvim.leader = "space"
+
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+
+lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>"
+lvim.keys.normal_mode["<C-d>"] = "<C-d>zz"
+lvim.keys.normal_mode["<C-u>"] = "<C-u>zz"
+lvim.keys.normal_mode["<C-a>"] = "ggVG"
+vim.opt.relativenumber = true
+
+vim.keymap.set('n', 'gn', ":tabe %<CR>")
+lvim.lsp.buffer_mappings.normal_mode["gr"] = {
+  ":lua require'telescope.builtin'.lsp_references()<cr>",
+  kind.cmp_kind.Reference .. " Find references"
+}
+
+lvim.lsp.buffer_mappings.normal_mode["gd"] = {
+  ":lua vim.lsp.buf.definition()<cr>",
+  -- ":lua require'telescope.builtin'.lsp_definitions()<cr>",
+  kind.cmp_kind.Reference .. " Definitions"
+}
+lvim.keys.normal_mode["<C-b>"] = ":TmuxNavigateLeft<CR>"
+lvim.lsp.buffer_mappings.normal_mode["gD"] = {
+  ":lua vim.lsp.buf.type_definition()<cr>",
+  kind.cmp_kind.Reference .. " Type Definition"
+}
+
+lvim.lsp.buffer_mappings.normal_mode["gf"] = {
+  ":Telescope frecency<cr>",
+  kind.cmp_kind.Reference .. " Telescope Frecency"
+}
