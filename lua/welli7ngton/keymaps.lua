0eaf20b20ad6949414cb9941e113107e0b5b7eba
Wellington - Trabalho <wellingtonasilva45@gmail.com>
2024-10-02 15:05:39 -0300

    chore: adiciona novos comandos e remove antigos

--- lua/welli7ngton/keymaps.lua
+++ lua/welli7ngton/keymaps.lua
@@ -18,10 +18,15 @@ lvim.keys.normal_mode["<c-u>"] = "<c-u>zz"
 -- seleciona o arquivo todo ctrl-a
 lvim.keys.normal_mode["<c-a>"] = "ggVG"
 
+-- Cria uma aba nova referenciando o arquivo atual
+-- gt vai para proxima aba
+-- gT vai para aba anterior
+-- (123456789)gt escolhe a aba de acordo com o numero digitado
 vim.keymap.set('n', 'gn', ":tabe %<cr>")
+vim.keymap.set('n', 'gN', ":tabclose<cr>")
 
 lvim.lsp.buffer_mappings.normal_mode["gr"] = {
-  ":lua require'telescope.builtin'.lsp_References()<cr>",
+  ":lua require'telescope.builtin'.lsp_references()<cr>",
   kind.cmp_kind.Reference .. " find references"
 }
 
@@ -31,9 +36,6 @@ lvim.lsp.buffer_mappings.normal_mode["gd"] = {
   kind.cmp_kind.Reference .. " definitions"
 }
 
-lvim.keys.normal_mode["<c-b>"] = ":TmuxNavigateLeft<cr>"
-
--- Go to File
 lvim.lsp.buffer_mappings.normal_mode["gf"] = {
   ":Telescope frecency<cr>",
   kind.cmp_kind.Reference .. " telescope frecency"
@@ -45,3 +47,11 @@ vim.api.nvim_set_keymap('n', '<S-f>', ':Telescope live_grep<CR>', { noremap = tr
 -- Mapeamento para <leader>tt abrir o terminal usando ToggleTerm
 lvim.keys.normal_mode["<leader>tt"] = "<cmd>ToggleTerm<CR>"
 lvim.keys.term_mode["<leader>tt"] = "<cmd>ToggleTerm<CR>"
+
+-- Comandos para copiar conteudo do lunarvim para area de transferencia do sistema
+lvim.keys.normal_mode["<leader>y"] = '"+y'
+lvim.keys.visual_mode["<leader>y"] = '"+y'
+lvim.keys.normal_mode["<leader>Y"] = '"+Y'
+
+-- Remap do modo visual para colar sem sobrescrever o registro de cópia
+lvim.keys.visual_mode["<leader>p"] = [["_dP]]
