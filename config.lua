9667cffeeb36789a9a158bcde09c8b4eb32babf5
Wellington - Trabalho <wellingtonasilva45@gmail.com>
2024-10-30 15:24:31 -0300

    add: snippets for python, java typescript and sql

--- config.lua
+++ config.lua
@@ -3,25 +3,25 @@ require("welli7ngton.options")     -- Configurações gerais
 require("welli7ngton.plugins")     -- Plugins
 require("welli7ngton.colorscheme") -- Tema de cores
 require("welli7ngton.autocmds")    -- Comandos automáticos
-require("welli7ngton.filetype")    -- Configurações de tipos de arquivos
+-- require("welli7ngton.filetype")    -- Configurações de tipos de arquivos
 require("welli7ngton.dashboard")   -- Configuração de dashboard e banners
 require("welli7ngton.which-key")
 require("welli7ngton.keymaps")
 require("welli7ngton.general_lsp")
 require("welli7ngton.java-lsp")
--- robotframework lsp configs
 require("welli7ngton.robotframework-lsp")
 require("welli7ngton.statusline")
 -- Configurações gerais do LunarVim
 lvim.log.level = "warn"
 vim.opt.timeoutlen = 500
 lvim.format_on_save = true
-lvim.transparent_window = true
+lvim.transparent_window = false
+
+-- For more snippets
+-- And this site to make snippets: https://snippet-generator.app
+-- https://github.com/Hydraallen/lvim/blob/main/snippets/java.json
 
 -- C/C++ setup
 lvim.lsp.installer.setup.automatic_installation = true
 
 lvim.lsp.installer.setup.ensure_installed = { "clangd" }
-
--- Coloca o explorador de arquivos do lado direito
-lvim.builtin.nvimtree.setup.view.side = "right"
