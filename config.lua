467ce9695cd011524ddcb8e70833219040e6fbe5
welli7ngton <wellingtonasilva45@gmail.com>
2024-11-03 21:09:14 -0300

    plugin add: lvim-pomodorow

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ require("welli7ngton.options")     -- Configurações gerais
 require("welli7ngton.plugins")     -- Plugins
 require("welli7ngton.colorscheme") -- Tema de cores
 require("welli7ngton.autocmds")    -- Comandos automáticos
--- require("welli7ngton.filetype")    -- Configurações de tipos de arquivos
+require("welli7ngton.filetype")    -- Configurações de tipos de arquivos
 require("welli7ngton.dashboard")   -- Configuração de dashboard e banners
 require("welli7ngton.which-key")
 require("welli7ngton.keymaps")
@@ -15,7 +15,7 @@ require("welli7ngton.statusline")
 lvim.log.level = "warn"
 vim.opt.timeoutlen = 500
 lvim.format_on_save = true
-lvim.transparent_window = false
+-- lvim.transparent_window = false
 
 -- For more snippets
 -- And this site to make snippets: https://snippet-generator.app
